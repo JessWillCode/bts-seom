d6b6258197c6f512a279055c6292c020d2923be0
Jessica Williams <jesswillcode@gmail.com>
2025-07-05 00:48:16 -0400

    Update vite.config.js

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export default defineConfig({
     plugins: [react()],
@@ -13,7 +10,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             "/api": {
-                target: process.env.VITE_API_BASE_URL || "http://localhost:3001", // eslint-disable-line no-undef
+                target: "http://localhost:3001", // eslint-disable-line no-undef
                 changeOrigin: true,
                 secure: false,
             },
